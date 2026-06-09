ecda14b8f0add74ca69a0d09631033916e0eda14
i-am-vaibhav <vg566556@gmail.com>
2026-06-09 17:35:05 +0530

    Create Sutra shell and project architecture

---
+++ lib/features/models/models_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ModelsScreen extends StatelessWidget {
+  const ModelsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Models'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import 'features/chat/chat_screen.dart';
+import 'features/files/files_screen.dart';
+import 'features/models/models_screen.dart';
+import 'features/settings/settings_screen.dart';
+
+void main() {
+  runApp(const SutraApp());
+}
+
+class SutraApp extends StatelessWidget {
+  const SutraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sutra',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomeShell(),
+    );
+  }
+}
+
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends State<HomeShell> {
+  int currentIndex = 0;
+
+  final screens = const [
+    ChatScreen(),
+    FilesScreen(),
+    ModelsScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sutra'),
+      ),
+      body: screens[currentIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.chat_outlined),
+            label: 'Chat',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.folder_outlined),
+            label: 'Files',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.memory_outlined),
+            label: 'Models',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
